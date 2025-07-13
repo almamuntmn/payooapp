e95de009094ba09c8636ddf494c62b471083a37a
almamuntmn <almamuncool88@gmail.com>
2025-07-13 18:33:04 +0600

    second

---
+++ js/login.js
@@ -0,0 +1,15 @@
+document.getElementById('add-money-btn').addEventListener('click', function(event){
+    event.preventDefault;
+
+    const phoneNUmber = document.getElementById('phone-number').value;
+    console.log(phoneNUmber);
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(pinNumber);
+
+    if(phoneNUmber === '01946159273' && pinNumber === '1234'){
+        console.log('You are logged in');
+    }
+    else{
+        alert('Wrong Phone or Pin');
+    }
+})
